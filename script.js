e4ae065657b2d552d121293c1771f66ec1effb3c
PGold <pglz762@gmail.com>
2023-07-30 13:21:10 -0500

    Added S to pull from LocalStorage and place in TextArea on reload

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Save User Input to Local Storage
   $("button").click(function () {
     var hourId = $(this).closest(".time-block").attr("id");
     var description = $(this).siblings(".description").val();
@@ -20,14 +12,24 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+  // Set the info from Local Storage to Textareas
+  function loadUserInputFromLocalStorage() {
+    $(".time-block").each(function () {
+      var hourId = $(this).attr("id");
+      var description = localStorage.getItem(hourId);
+
+      if (description !== null) {
+        $(this).find(".description").val(description);
+      }
+    });
+  }
 
   // Sets te current Date in the <p> in header
   var now = dayjs(new Date());
   $('#currentDay').text(now.format('MMM D, YYYY'));
+
+  //loads the info from LocalStorage
+  loadUserInputFromLocalStorage();
 
 });
